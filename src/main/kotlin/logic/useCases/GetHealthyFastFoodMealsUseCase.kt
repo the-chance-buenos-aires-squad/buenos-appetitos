da78aefda008cbbc9561da1710cd31a8f233f3ce
AhmedMohamed056 <hamadarayyan056@gmail.com>
2025-04-17 12:47:56 +0200

    sorted items by Normalization

--- src/main/kotlin/logic/useCases/GetHealthyFastFoodMealsUseCase.kt
+++ src/main/kotlin/logic/useCases/GetHealthyFastFoodMealsUseCase.kt
@@ -1,27 +1,36 @@
 package org.example.logic.useCases
 
 import org.example.logic.RecipesRepository
+import org.example.model.Recipe
 
 class GetHealthyFastFoodMealsUseCase(
     private val recipesRepository:RecipesRepository
 ) {
-    fun getHealthyFastFood(count:Int):List<String>{
+    fun getHealthyFastFood():List<Recipe>{
         val allRecipes=recipesRepository.getRecipes()
-       val recipesPrepearedInFiftyMinutesOrLess= allRecipes
+        val normalizedRecipe=sortedByNormalization(allRecipes)
+       val recipesPreparedInFiftyMinutesOrLess= normalizedRecipe
            .filter {
                it.minutes<=15&&
-               it.nutrition[1]<=17&&
-               it.nutrition[5]<=5&&
-               it.nutrition[6]<=35&&
                it.nutrition[1] > 0&&
                it.nutrition[5] > 0&&
                it.nutrition[6] > 0
            }
-           .sortedBy {
-               (it.nutrition[1]*0.2+it.nutrition[5]*0.6+it.nutrition[6]*0.1)/3
-           }
-           .take(count)
-           .map { "${it.name} total fat ${it.nutrition[1]} saturated fat ${it.nutrition[5]} Carbohydrates ${it.nutrition[6]}" }
-        return recipesPrepearedInFiftyMinutesOrLess
+           .take(10)
+        return recipesPreparedInFiftyMinutesOrLess
+    }
+    private fun sortedByNormalization(allRecipes:List<Recipe>):List<Recipe>{
+        val minFat=allRecipes.minOf { it.nutrition[1] }
+        val maxFat=allRecipes.maxOf { it.nutrition[1] }
+        val minSaturatedFat=allRecipes.minOf { it.nutrition[5] }
+        val maxSaturatedFat=allRecipes.maxOf { it.nutrition[5] }
+        val minCarbs=allRecipes.minOf { it.nutrition[6] }
+        val maxCarbs=allRecipes.maxOf { it.nutrition[6] }
+        return allRecipes.sortedBy {
+            val fatNorm = (it.nutrition[1] - minFat) / (maxFat - minFat)
+            val saturatedNorm = (it.nutrition[5] - minSaturatedFat) / (maxSaturatedFat - minSaturatedFat)
+            val carbsNorm = (it.nutrition[6] - minCarbs) / (maxCarbs - minCarbs)
+            fatNorm+saturatedNorm+carbsNorm
+        }
     }
 }
