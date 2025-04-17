ba101a761ee52871f18a1277b1635e679f367575
Abdelrahman Elshareif <Abdelrahmanelshareif@gmail.com>
2025-04-17 13:11:00 +0300

    add function "playIngredientGame" that start play

--- src/main/kotlin/logic/useCases/UseCaseHolder.kt
+++ src/main/kotlin/logic/useCases/UseCaseHolder.kt
@@ -4,6 +4,7 @@ import org.example.logic.RecipesRepository
 import java.util.*
 
 class UseCaseHolder(val repository:RecipesRepository) {
+    val ingredientGameUseCase = IngredientGameUseCase(repository)
     fun useCaseImplementation() {
 
     }
