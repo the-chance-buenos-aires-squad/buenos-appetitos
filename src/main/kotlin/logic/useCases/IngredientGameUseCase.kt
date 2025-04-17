321dc47e93ffd1e370df0902b6b43864109d7143
Abdelrahman Elshareif <Abdelrahmanelshareif@gmail.com>
2025-04-17 13:06:13 +0300

    add function "generateRound" that return option and the correct answer

--- src/main/kotlin/logic/useCases/IngredientGameUseCase.kt
+++ src/main/kotlin/logic/useCases/IngredientGameUseCase.kt
@@ -11,4 +11,21 @@ class IngredientGameUseCase(private val repository: RecipesRepository) {
         val options: List<String>,
         val correct: String
     )
+    fun generateRound(): Round {
+        val meal = allMeals.random()
+        val correct = meal.ingredients.random()
+
+        val wrongOptions = allIngredients
+            .filterNot { it.equals(correct, ignoreCase = true) }
+            .shuffled()
+            .take(2)
+
+        val allOptions = (wrongOptions + correct).shuffled()
+
+        return Round(
+            meal = meal,
+            options = allOptions,
+            correct = correct
+        )
+    }
 }
