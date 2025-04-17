6814d9fbeecd42ed61a600b31d54cfba3deeb220
moyasser hazim <measer100@gmail.com>
2025-04-17 19:55:02 +0300

    inject the recipe parser

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,13 +2,12 @@ package org.example
 
 import org.example.data.CsvFileReader
 import org.example.data.CsvRecipesRepository
+import org.example.data.RecipeParser
 import org.example.logic.useCases.UseCaseHolder
 import org.example.presentation.HolderCLi
 
 fun main() {
     val csvFileReader = CsvFileReader()
-    val repository = CsvRecipesRepository(csvFileReader)
-    val useCases = UseCaseHolder(repository)
-    val holderCli = HolderCLi()
-    holderCli.startCLI(useCases)
+    val recipeParser = RecipeParser()
+    val repository = CsvRecipesRepository(csvFileReader, recipeParser)
 }
