e9dc2dc4e43fb99e6938a07bf6ade1013d35c5b3
NadeenRR <nadeenradwan0@gmail.com>
2025-04-18 11:44:23 +0300

    - update love potato use case with add extension function and return throw exception - update find potato dishes fun in holder cli file

--- src/main/kotlin/logic/useCases/LovePotatoUseCase.kt
+++ src/main/kotlin/logic/useCases/LovePotatoUseCase.kt
@@ -5,14 +5,21 @@ class LovePotatoUseCase(private val repository: RecipesRepository) {
 
     fun getRandomPotatoRecipes(): List<Recipe> {
         val allRecipes = repository.getRecipes()
-        val potatoRecipes = allRecipes.filter { recipe ->
-            recipe.ingredients.any { ingredient ->
-                ingredient.contains("potato", ignoreCase = true) ||
-                        ingredient.contains("potatoes", ignoreCase = true)
-            }
-        }
+
+        if (allRecipes.isEmpty()) throw Exception("No recipes found")
+
+        val potatoRecipes = allRecipes.potatoLoverList()
+
         return if (potatoRecipes.size <= 10) potatoRecipes
         else potatoRecipes.shuffled().take(10)
+    }
+}
 
+private fun List<Recipe>.potatoLoverList(): List<Recipe> {
+    return this.filter { recipe ->
+        recipe.ingredients.any { ingredient ->
+            ingredient.contains("potato", ignoreCase = true) ||
+                    ingredient.contains("potatoes", ignoreCase = true)
+        }
     }
 }
