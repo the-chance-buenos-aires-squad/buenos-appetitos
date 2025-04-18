5bc348b22136ce82a1652d4cb8e6d335a8d67159
Abdelrahman Elshareif <Abdelrahmanelshareif@gmail.com>
2025-04-18 18:44:59 +0300

    fix

---
+++ src/main/kotlin/model/RecipeIndices.kt
@@ -0,0 +1,16 @@
+package org.example.model
+
+object RecipeIndices {
+    const val NAME_INDEX = 0
+    const val ID_INDEX = 1
+    const val MINUTES_INDEX = 2
+    const val CONTRIBUTOR_ID_INDEX = 3
+    const val SUBMITTED_INDEX = 4
+    const val TAGS_INDEX = 5
+    const val NUTRITION_INDEX = 6
+    const val NUMBER_OF_STEPS_INDEX = 7
+    const val STEPS_INDEX = 8
+    const val DESCRIPTION_INDEX = 9
+    const val INGREDIENTS_INDEX = 10
+    const val NUMBER_OF_INGREDIENTS_INDEX = 11
+}
