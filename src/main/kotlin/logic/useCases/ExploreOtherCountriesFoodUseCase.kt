5d5ab58485a8e75beaa45f088f01d1c21dc09416
Abdulaziz Anwer <the.azizanwer@gmail.com>
2025-04-18 18:15:10 +0300

    add logic usecase for exploring other countries food

---
+++ src/main/kotlin/logic/useCases/ExploreOtherCountriesFoodUseCase.kt
@@ -0,0 +1,23 @@
+package org.example.logic.useCases
+
+import org.example.logic.RecipesRepository
+import org.example.model.Recipe
+
+class ExploreOtherCountriesFoodUseCase(private val repository: RecipesRepository) {
+
+    fun searchCountryName(userInput: String): List<Recipe> {
+        val country = userInput.trim().lowercase()
+        val allRecipes = repository.getRecipes()
+        if (allRecipes.isEmpty()) throw Exception("Country name cannot be empty")
+        val matchedRecipes = allRecipes.countryNamesList(country)
+        if (matchedRecipes.isEmpty()) throw Exception("No meals found for '$country")
+        return matchedRecipes.shuffled().take(20)
+    }
+}
+
+private fun List<Recipe>.countryNamesList(country: String): List<Recipe> {
+    return this.filter { recipe ->
+        recipe.tags.any { it.contains(country, ignoreCase = true) } ||
+                (recipe.description?.contains(country, ignoreCase = true) ?: false)
+    }
+}
