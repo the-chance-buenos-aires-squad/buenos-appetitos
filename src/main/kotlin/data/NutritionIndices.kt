912d7d1a2dd6a1c4ebe93710c7e90e5a03093dcd
mostafa nema rasheed <mostafa.n3ma@gmail.com>
2025-04-21 11:19:52 +0300

    refactor: - refactor start function for get keto recipe use case - create get keto recipe Cli - moving input handling logic to package input handling utilities

--- src/main/kotlin/data/NutritionIndices.kt
+++ src/main/kotlin/data/NutritionIndices.kt
@@ -9,3 +9,63 @@ object NutritionIndices {
     const val SATURATED_FAT_INDEX = 5
     const val CARBOHYDRATES_INDEX = 6
 }
+
+
+
+
+//package org.example.presentation
+//
+//import org.example.logic.useCases.SweetsWithNoEggsUseCase
+//import org.example.model.Recipe
+//import org.example.presentation.displyUtils.displayDetails
+//
+//class SweetsWithNoEggsCLi(private val sweetsWithNoEggsUseCase: SweetsWithNoEggsUseCase) {
+//
+//    fun start() {
+//        println("\n------ Sweet without egg -----")
+//        showMenuLoop()
+//    }
+//
+//    private fun showMenuLoop() {
+//        var continueLoop = true
+//
+//        while (continueLoop) {
+//            val dessert = getRandomEggFreeDessert()
+//
+//            println("\n1 - Like this dessert")
+//            println("2 - Dislike (show another option)")
+//            println("3 - Exit")
+//
+//            print("Please choose (1-3): ")
+//            val userChoice = readln().toIntOrNull()
+//
+//            when (userChoice) {
+//                1 -> {
+//                    println("Thank you for your choice!  \n${dessert.displayDetails()}")
+//                    continueLoop = false
+//                }
+//
+//                2 ->{}
+//                3 -> {
+//                    println("Exiting...")
+//                    continueLoop = false
+//                }
+//
+//                else -> println("Invalid input. Please enter 1 = Like , 2 = Dislike, or 3 = Exit.")
+//            }
+//        }
+//    }
+//
+//    //region method helper for SweetWithNoEggs
+//    private fun getRandomEggFreeDessert(): Recipe {
+//        return try {
+//            val meal = sweetsWithNoEggsUseCase.getRandomSweetsNoEggs()
+//            println("|| Name: ${meal.name} || Description: ${meal.description}")
+//            meal
+//        } catch (exception: Exception) {
+//            println("Error: ${exception.message}")
+//            throw exception
+//        }
+//    }
+//
+//}
