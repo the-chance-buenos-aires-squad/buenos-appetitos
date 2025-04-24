a2d5dd51ec8069ed103d9ba3cda90c24fbb32618
Sara Desouki <saradesouki39@gmail.com>
2025-04-24 18:59:53 +0200

    make reformat for code and edit setup function name and use truth for assert

--- src/test/kotlin/logic/useCases/IdentifyIraqiRecipesUseCaseTest.kt
+++ src/test/kotlin/logic/useCases/IdentifyIraqiRecipesUseCaseTest.kt
@@ -1,5 +1,6 @@
 package logic.useCases
 
+import com.google.common.truth.Truth.assertThat
 import dummyData.DummyRecipes
 import dummyData.DummyTags
 import io.mockk.every
@@ -10,42 +11,45 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
-class IdentifyIraqiRecipesUseCaseTest{
+class IdentifyIraqiRecipesUseCaseTest {
  private lateinit var iraqiRecipesUseCase: IdentifyIraqiRecipesUseCase
-  private val repository: RecipesRepository = mockk(relaxed = true)
+ val repository: RecipesRepository = mockk()
 
-  @BeforeEach
-  fun stetup(){
-   iraqiRecipesUseCase = IdentifyIraqiRecipesUseCase(repository)
-  }
- @Test
- fun `should return iraqi recipe when pass all recipes `(){
-  //given
-  every{ repository.getRecipes()} returns DummyRecipes.iraqiRecipeOptions
-  //when
-   val result = iraqiRecipesUseCase.getIraqiRecipes()
-  //then
-  assertEquals(2,result.size)
+ @BeforeEach
+ fun stetUp() {
+  iraqiRecipesUseCase = IdentifyIraqiRecipesUseCase(repository)
  }
+
+ @Test
+ fun `should return iraqi recipe when pass all recipes `() {
+  //given
+  every { repository.getRecipes() } returns DummyRecipes.iraqiRecipeOptions
+  //when
+  val result = iraqiRecipesUseCase.getIraqiRecipes()
+  //then
+  assertThat(result).isEqualTo(2)
+ }
+
  @Test
  fun `should return empty list of iraqi recipe  when no recipes found `() {
   // Given
-  every {  repository.getRecipes()} returns emptyList()
+  every { repository.getRecipes() } returns emptyList()
 
   // When
   val result = iraqiRecipesUseCase.getIraqiRecipes()
 
   // Then
-  assertEquals(0, result.size)
+  assertThat(result).isEqualTo(0)
  }
+
  @Test
- fun `should return list of iraqi recipes when description and tag for recipe contain iraqi word `(){
+ fun `should return list of iraqi recipes when description and tag for recipe contain iraqi word `() {
   // Given
   DummyRecipes.iraqiRecipeOptions
   //when
   val result = DummyRecipes.iraqiRecipeOptions.filterByCountry("iraqi")
   //then
-  assertEquals(2,result.size)
+  assertThat(result).isEqualTo(2)
  }
 
- }
+}
