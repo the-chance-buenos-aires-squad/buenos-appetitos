a2c18a77dbf7fdb5e8316334d391103f35e75968
moyasser hazim <measer100@gmail.com>
2025-04-24 20:22:51 +0300

    update inject class

--- src/main/kotlin/logic/useCases/RandomRecipeUseCase.kt
+++ src/main/kotlin/logic/useCases/RandomRecipeUseCase.kt
@@ -1,12 +1,13 @@
 package org.example.logic.useCases
 
 import org.example.data.CsvRecipesRepository
+import org.example.logic.RecipesRepository
 import org.example.model.Recipe
 
-class RandomRecipeUseCase(private val csvRecipesRepository: CsvRecipesRepository) {
+class RandomRecipeUseCase(private val repository: RecipesRepository) {
 
      fun getRandomRecipe(): Recipe {
-        return csvRecipesRepository.getRecipes().random()
+        return repository.getRecipes().random()
     }
 
 }
