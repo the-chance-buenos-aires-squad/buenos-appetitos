4214d39bef474c5d6c904f37fcf917dd066d5392
moyasser hazim <measer100@gmail.com>
2025-04-24 19:55:22 +0300

    move logic to use case from cli

--- src/main/kotlin/logic/useCases/GuessGameUseCase.kt
+++ src/main/kotlin/logic/useCases/GuessGameUseCase.kt
@@ -1,25 +1,44 @@
 package org.example.logic.useCases
 
-import org.example.logic.RecipesRepository
+import org.example.model.GuessGameState
 import org.example.model.GuessAttemptState
 
-class GuessGameUseCase(private val repository: RecipesRepository ) {
+class GuessGameUseCase(
+    private val randomRecipeUseCase: RandomRecipeUseCase
+) {
 
-
-     fun submit(userGuess: Int, preparationTime: Int, attempts: Int): GuessAttemptState {
-        return if (userGuess == preparationTime)
-            GuessAttemptState.CORRECT
-        else if (attempts == MAX_ATTEMPTS)
-            throw  Exception("Game Over! The correct preparation time is $preparationTime minutes")
-        else if (userGuess < preparationTime)
-            GuessAttemptState.TOO_LOW
-        else
-            GuessAttemptState.TOO_HIGH
+    fun startGame(): GuessGameState {
+      return GuessGameState(recipe = randomRecipeUseCase.getRandomRecipe())
     }
 
+    fun processGuess(state: GuessGameState, userGuess: Int?): Pair<GuessGameState, GuessAttemptState?> {
+        if (userGuess == null) {
+            return state to null
+        }
+
+        val newAttemptCount = state.attemptCount + 1
+        val result = evaluateGuess(userGuess, state.recipe.minutes, newAttemptCount)
+
+        val newState = when (result) {
+            is GuessAttemptState.Correct, is GuessAttemptState.GameOver -> {
+                state.copy(attemptCount = newAttemptCount, isFinished = true)
+            }
+
+            else -> state.copy(attemptCount = newAttemptCount)
+        }
+
+        return newState to result
+    }
+
+    private fun evaluateGuess(userGuess: Int, preparationTime: Int, attemptCount: Int): GuessAttemptState =
+        when {
+            userGuess == preparationTime -> GuessAttemptState.Correct
+            attemptCount >= MAX_ATTEMPTS -> GuessAttemptState.GameOver(preparationTime)
+            userGuess < preparationTime -> GuessAttemptState.TooLow
+            else -> GuessAttemptState.TooHigh
+        }
 
     companion object {
         const val MAX_ATTEMPTS = 3
     }
 }
-
