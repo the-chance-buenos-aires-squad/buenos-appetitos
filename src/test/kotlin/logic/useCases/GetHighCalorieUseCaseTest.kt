d77e708668f9ae08ff42a69503cf021eaa1c0c52
NadeenRR <nadeenradwan0@gmail.com>
2025-04-25 17:33:47 +0300

    - add test cases for high calorie use cas - pass all test cases with 100% coverage result

--- src/test/kotlin/logic/useCases/GetHighCalorieUseCaseTest.kt
+++ src/test/kotlin/logic/useCases/GetHighCalorieUseCaseTest.kt
@@ -1,7 +1,81 @@
 package logic.useCases
 
+import com.google.common.truth.Truth.assertThat
+import dummyData.DummyNutrition
+import dummyData.DummyRecipes
+import dummyData.createDummyRecipe
+import io.mockk.every
+import io.mockk.mockk
+import org.example.logic.RecipesRepository
+import org.example.logic.useCases.GetHighCalorieUseCase
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.Test
 
-class GetHighCalorieUseCaseTest {
+class GetHighCalorieUseCaseTest{
+    private var repository: RecipesRepository = mockk(relaxed = true)
+    private lateinit var getHighCalorieUseCase: GetHighCalorieUseCase
 
+    @BeforeEach
+    fun setUp() {
+        getHighCalorieUseCase = GetHighCalorieUseCase(repository)
+    }
+
+    @Test
+    fun `should throw exception when no recipes are found`() {
+        // given
+        every { repository.getRecipes() } returns emptyList()
+
+        // when and then
+        assertThrows<IllegalStateException> {
+            getHighCalorieUseCase.suggestRandomHighCalorieRecipe()
+        }
+    }
+
+    @Test
+    fun `should return a random high calorie recipe from the list when available`() {
+
+        // given
+        every { repository.getRecipes() } returns DummyRecipes.highCalorieRecipes
+
+        // when
+        val result = getHighCalorieUseCase.suggestRandomHighCalorieRecipe()
+
+        // then
+        assertThat(result).isNotNull()
+        assertTrue(result in DummyRecipes.highCalorieRecipes)
+    }
+
+    @Test
+    fun `should filter out low-calorie recipes`() {
+        // given
+        every { repository.getRecipes() } returns
+                DummyRecipes.lowCalorieRecipes + DummyRecipes.highCalorieRecipes
+
+        // when
+        val result = getHighCalorieUseCase.suggestRandomHighCalorieRecipe()
+
+        // then
+        assertTrue(result.nutrition.calories > 700)
+    }
+
+    @Test
+    fun `should remove suggested recipe from available recipes when suggesting it`() {
+        // given
+       val firstRecipeWithHighCalories =  createDummyRecipe( name = "High Calorie 1", nutrition = DummyNutrition.highCalorie)
+       val secondRecipeWithHighCalories = createDummyRecipe( name = "High Calorie 2", nutrition = DummyNutrition.highCalorie)
+
+        val recipesWithHighCalories = listOf(firstRecipeWithHighCalories, secondRecipeWithHighCalories)
+        every { repository.getRecipes() } returns recipesWithHighCalories
+
+        // when
+        val firstResult = getHighCalorieUseCase.suggestRandomHighCalorieRecipe()
+        val secondResult = getHighCalorieUseCase.suggestRandomHighCalorieRecipe()
+
+        // then
+        assertNotEquals(firstResult, secondResult)
+        assertTrue(recipesWithHighCalories.contains(firstResult))
+        assertTrue(recipesWithHighCalories.contains(secondResult))
+    }
 }
